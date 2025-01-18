ad91545ba44298b803e74bc904be0234741aea70
Fritsl <fritslyneborg@gmail.com>
2025-01-18 20:22:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TopMenu } from './components/layout/TopMenu';
 import { Footer } from './components/layout/Footer';
 import { JobSearch } from './pages/JobSearch';
 import { CompanySearch } from './pages/CompanySearch';
-import { ProfileForm } from './pages/ProfileForm';
+import { identityForm } from './pages/identityForm';
 import { Tools } from './pages/Tools';
 import { HowItWorks } from './pages/HowItWorks';
 import { Privacy } from './pages/Privacy';
@@ -12,14 +12,14 @@ import { About } from './pages/About';
 import { Identities } from './pages/Identities';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
-import { ProfileProvider } from './contexts/ProfileContext';
+import { identityProvider } from './contexts/identityContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
 
 function App() {
   return (
     <AuthProvider>
-      <ProfileProvider>
+      <identityProvider>
         <Router>
           <div className="min-h-screen bg-background flex flex-col">
             <TopMenu />
@@ -35,7 +35,7 @@ function App() {
                 {/* Protected Routes */}
                 <Route path="/" element={
                   <ProtectedRoute>
-                    <ProfileForm />
+                    <identityForm />
                   </ProtectedRoute>
                 } />
                 <Route path="/jobs" element={
@@ -66,7 +66,7 @@ function App() {
             <Footer />
           </div>
         </Router>
-      </ProfileProvider>
+      </identityProvider>
     </AuthProvider>
   );
 }
